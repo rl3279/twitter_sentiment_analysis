d9e7be97588c42fac576cc025d7950eaba853b70
rl3279 <rl3279@columbia.edu>
2023-03-27 00:51:37 -0400

    Added script to sampling from data

---
+++ get_sub_dataset.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+import my_globals
+
+# Always save main dataset outside of the code repo.
+# In this case, my data_dir is "../project_data" parallel to "."
+
+def get_sub_dataset(size:int=5000, random_seed:int = 0):
+    """Generate a subset of the main dataset usinga random seed.
+    Saves sub-dataset to a diectory outside of and parallel to main directory. 
+    
+    :param size: size of subset
+    :type size: int, optional. Default to be 5000.
+    :param random_seed: random seed
+    :type random_seed: int, optional. Default to be 0.
+    """
+    DATA_PATH = "/".join([my_globals.DATA_DIR, my_globals.MAIN_DATA_NAME])
+    DATA_PATH = "../project_data/training.1600000.processed.noemoticon.csv"
+    data = pd.read_csv(DATA_PATH, encoding = 'ISO-8859-1', header = None, names = my_globals.HEADER)
+    idx = np.random.choice(data.index, size)
+    data.loc[idx].to_csv("/".join([my_globals.DATA_DIR, f"twitter_seed{random_seed}.csv"]), index = False)
+    
---
+++ my_globals.py
@@ -0,0 +1,3 @@
+DATA_DIR = "../project_data"
+MAIN_DATA_NAME = "training.1600000.processed.noemoticon.csv"
+HEADER = ["target", "ids", "date", "flag", "user", "text"]
