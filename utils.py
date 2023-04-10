66d9c0b207465a78d6b038ac01cdbfd14733dcb7
rl3279 <rl3279@columbia.edu>
2023-04-09 22:14:47 -0400

    Linting

--- utils.py
+++ utils.py
@@ -8,10 +8,11 @@ from preprocessing import cleaning, preprocess_pipeline
 # Always save main dataset outside of the code repo.
 # In this case, my data_dir is "../project_data" parallel to "."
 
-def get_sub_dataset(size:int=5000, random_seed:int = 0) -> pd.DataFrame:
+
+def get_sub_dataset(size: int = 5000, random_seed: int = 0) -> pd.DataFrame:
     """Generate a subset of the main dataset usinga random seed.
     Saves sub-dataset to a diectory outside of and parallel to main directory. 
-    
+
     :param size: size of subset
     :type size: int, optional. Default to be 5000.
     :param random_seed: random seed
@@ -21,22 +22,23 @@ def get_sub_dataset(size:int=5000, random_seed:int = 0) -> pd.DataFrame:
     DATA_PATH = "/".join([my_globals.DATA_DIR, my_globals.MAIN_DATA_NAME])
     DATA_PATH = "../project_data/training.1600000.processed.noemoticon.csv"
     data = pd.read_csv(
-        DATA_PATH, 
+        DATA_PATH,
         # encoding = 'ISO-8859-1',
-        encoding = "latin1",
-        header = None, 
-        names = my_globals.HEADER
+        encoding="latin1",
+        header=None,
+        names=my_globals.HEADER
     )
     idx = np.random.choice(data.index, size)
     data = data.loc[idx]
     data.to_csv(
-        "/".join([my_globals.DATA_DIR, f"twitter_seed{random_seed}.csv"]), 
-        index = False,
-        encoding = "latin1"
+        "/".join([my_globals.DATA_DIR, f"twitter_seed{random_seed}.csv"]),
+        index=False,
+        encoding="latin1"
     )
     return data
-    
-def get_sub_featured_datasets(size:int=5000, random_seed:int = 0) -> pd.DataFrame:
+
+
+def get_sub_featured_datasets(size: int = 5000, random_seed: int = 0) -> pd.DataFrame:
     data = get_sub_dataset(size, random_seed)
     data = cleaning(data)
     data["processed_text"] = data["text"].apply(preprocess_pipeline)
@@ -44,5 +46,5 @@ def get_sub_featured_datasets(size:int=5000, random_seed:int = 0) -> pd.DataFram
     data["mention_count"] = data["text"].apply(fe.mention_count)
     data["cap_freq"] = data["text"].apply(fe.cap_freq)
     count_tfidf = fe.get_token_features(data, "both")
-    data = pd.concat([data, count_tfidf], axis = 1)
+    data = pd.concat([data, count_tfidf], axis=1)
     return data
